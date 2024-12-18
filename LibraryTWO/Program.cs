e83cbb6eaee8ed9eba9827aff0b96d6a9a08133c
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-12-18 22:07:31 +0500

    parametereized queries edited

--- LibraryTWO/Program.cs
+++ LibraryTWO/Program.cs
@@ -11,8 +11,13 @@ namespace LibraryTWO
     {
         static void Main(string[] args)
         {
-            Library.Insert("Authors", "author_id, last_name, first_name", "9, 'Stroustrup', 'Bjarne'");
-     
+            //Library.Insert("Authors", "author_id, last_name, first_name", "11, 'Stroustrup', 'Bjarne'");
+            //Library.InsertAuthor(12, "Randy", "Gadge");
+            //Console.WriteLine(Library.GetAuthorID("Scott Muller"));
+            //Library.InsertAuthor(13, "Stroustrup", "Bjarne");
+
+
+            Library.InsertBook(7, "C++ Programming Language", 331, "1986-01-29", "Bjarne Stroustroup");
 
             Library.Select("author_id, first_name, last_name", "Authors");
             Library.Select("book_title, publish_date ,[Author]=first_name+ ' ' + last_name", "Authors,Books", "author = author_id", 34);
